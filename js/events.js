5f74ff84ab6a0a4071c11b782acbef4762b578ea
King Yorbi <yorbi12@gmail.com>
2020-01-15 15:17:54 -0300

    Primer commit

---
+++ js/events.js
@@ -0,0 +1,12 @@
+const ham = document.getElementById('ham')
+ham.addEventListener('click', function () {
+	document.body.classList.toggle('nav-is-toggled')
+})
+
+wow = new WOW ({
+	boxClass : 'wow',
+})
+wow.init();
+
+  
+  
